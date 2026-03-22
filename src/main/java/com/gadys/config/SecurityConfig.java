6470afa34bd36e945e2991dff3839db5a0830b30
yasmincunegundes25 <yasmincunegundes25@gmail.com>
2026-03-22 23:50:50 +0000

    Fix: SecurityConfig

--- src/main/java/com/gadys/config/SecurityConfig.java
+++ src/main/java/com/gadys/config/SecurityConfig.java
@@ -1,3 +1,18 @@
+package com.gadys.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.List;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
