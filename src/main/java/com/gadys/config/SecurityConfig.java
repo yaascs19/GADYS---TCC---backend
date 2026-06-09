7866621f103c12ddcadc36da695036065e94f5a5
yaascs19 <yasmincunegundes25@gmail.com>
2026-06-09 18:06:52 +0000

    Fix: changes on Cors

--- src/main/java/com/gadys/config/SecurityConfig.java
+++ src/main/java/com/gadys/config/SecurityConfig.java
@@ -1,19 +1,24 @@
 package com.gadys.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Autowired
+    private CorsConfigurationSource corsConfigurationSource;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .cors().and()
+            .cors(cors -> cors.configurationSource(corsConfigurationSource))
             .csrf().disable()
             .authorizeHttpRequests()
                 .antMatchers("/h2-console/**").permitAll()
